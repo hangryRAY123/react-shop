4c0ce1bf29141be1e82176c8b2af2814063da6ac
Павел <pasha.seregin.92@mail.ru>
2022-12-23 20:53:02 +0300

    gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ export const Footer = () => (
     <div className='footer-copyright'>
       <div className='container'>
         © {new Date().getFullYear()} Copyright Text
-        <a className='grey-text text-lighten-4 right' href='#!'>
+        <a
+          className='grey-text text-lighten-4 right'
+          href='https://github.com/hangryRAY123/react-shop'
+          target='_blank'
+          rel='noreferrer'
+        >
           Repo
         </a>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 export const Header = () => (
   <nav className='red darken-3'>
     <div className='nav-wrapper'>
-      <a href='!#' className='brand-logo'>
+      <a href='/' className='brand-logo'>
         React Shop
       </a>
-      <ul id='nav-mobile' className='right hide-on-med-and-down'>
+      <ul
+        id='nav-mobile'
+        className='right hide-on-med-and-down'
+      >
         <li>
-          <a href='!#'>Repo</a>
+          <a
+            href='https://github.com/hangryRAY123/react-shop'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Repo
+          </a>
         </li>
       </ul>
     </div>
